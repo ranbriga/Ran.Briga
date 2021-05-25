7e53dfca299dac58a1e56c777de1afbb440641c3
ranbriga <80330631+ranbriga@users.noreply.github.com>
2021-05-25 20:36:14 +0300

    Update app.py

--- CV/app.py
+++ CV/app.py
@@ -1,22 +1,24 @@
-from flask import Flask, url_for, redirect
-from flask import render_template
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/main', methods=['PUT', 'POST', 'GET'])
 @app.route('/', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def home():
     return render_template('cv.html')
 
-@app.route('/ContactList', methods=['GET', 'POST', 'DELETE', 'PUT'])
+@app.route('/ContactList.html', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def my_contactList():
     return render_template('ContactList.html')
 
 @app.route('/assignment8', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def my_assignment8():
+    contact_user = {'firstname': 'Ran', 'lastname': 'Briga'}
+    hobbies=['FreeDiving', 'Football', 'Guitar']
     return render_template('assignment8.html',
-                           user={'name': "Ran Briga"},
-                           hobbies=['FreeDiving', 'Football', 'Guitar'])
+                           hobbies=hobbies,
+                           contact_user=contact_user)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
